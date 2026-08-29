12a7c67f3d45a2f3cbc8fb86a1fe77da72f7b9e6
simao.lopes98 <simaolopes98@gmail.com>
2026-08-29 20:43:13 +0100

    02_func: add func exercise

---
+++ 02_func/func.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int readNumber (void)
+{
+    int input = 0;
+    cout << "Input a number\n";
+    cin >> input;
+    return input;
+}
+
+void displayAns (int ans)
+{
+    printf ("Answer is: %d\n", ans);
+}
+
+int main (void)
+{
+    displayAns (readNumber() + readNumber());
+    return 0;
+}

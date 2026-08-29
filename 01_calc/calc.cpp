e4515ff2d952a72774956d97dfd20014b5021aeb
simao.lopes98 <simaolopes98@gmail.com>
2026-08-29 20:43:13 +0100

    01_calc: add calc exercise

---
+++ 01_calc/calc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    int a = 0;
+    int b = 0;
+
+    cout << "Enter value 1\n";
+    cin >> a;
+    cout << "Enter value 2\n";
+    cin >> b;
+
+    printf ("Cal value %d\n", a+b);
+
+    return 0;
+
+}
